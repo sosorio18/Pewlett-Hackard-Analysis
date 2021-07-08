35d67435818f76066be15a634f703cc58306943f
sosorio18 <84107233+sosorio18@users.noreply.github.com>
2021-07-07 20:17:23 -0400

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,3 +29,21 @@ SELECT COUNT(ut.emp_no), ut.title
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.emp_no) DESC;
+
+
+-- Deliverable 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+-- INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON(e.emp_no=de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-1-1'AND '1965-12-31')
+ORDER BY e.emp_no;
